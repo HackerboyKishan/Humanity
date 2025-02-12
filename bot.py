33029d4dc9dd4999c7a130c2ce3a00e5d74c6466
HackerboyKishan <kishan74580@gmail.com>
2025-02-12 21:21:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ def process_claim(sender_address, private_key, web3, contract):
         })
         signed_txn = web3.eth.account.sign_transaction(transaction, private_key=private_key)
         tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-        tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash, timeout=10)
+        tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
         print(Fore.GREEN + f"Transaction successful for {sender_address} with hash: {web3.to_hex(tx_hash)}")
 
     except Exception as e:
